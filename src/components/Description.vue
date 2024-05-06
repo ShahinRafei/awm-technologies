647ea10b9b555a70a15829526d795b2a18c5c2e5
ShahinRafei <rafeishahin@gmail.com>
2024-05-06 04:41:39 +0330

    make description component based on test file

--- src/components/Description.vue
+++ src/components/Description.vue
@@ -1,3 +1,51 @@
+<script>
+export default {
+    // data
+    data() {
+        return {
+            description: '',
+        };
+    },
+
+    // methods
+    methods: {
+        isBalanced(text = this.description) {
+            const stack = [];
+            const openingBrackets = ['(', '[', '{'];
+            const closingBrackets = [')', ']', '}'];
+            const bracketMap = {
+                ')': '(',
+                ']': '[',
+                '}': '{',
+            };
+
+            for (let i = 0; i < text.length; i++) {
+                let char = text[i];
+                if (openingBrackets.includes(char)) {
+                    stack.push({ bracket: char, position: i });
+                } else if (closingBrackets.includes(char)) {
+                    const top = stack.pop();
+                    if (!top || top.bracket !== bracketMap[char]) {
+                        return false;
+                    }
+                }
+            }
+
+            return stack.length === 0;
+        },
+
+        checkForQuestionMark(event) {
+            const newText = event.target.value;
+            if (newText.includes('?')) {
+                event.target.nextSibling.innerText = this.isBalanced(newText) ? 'The text is balanced.' : 'The text is not balanced.';
+            }
+        }
+    },
+
+};
+</script>
+
 <template>
-  z
+    <input v-model="description" @input="checkForQuestionMark" type="text" id="description">
+    <p></p>
 </template>
